1b1b8105c6bdf9a4a07b0899f8f1af38ee3daee0
francisco leal <franciscoleal152000@gmail.com>
2026-01-21 17:05:43 -0300

    Mobile first version complete

--- src/app/premium/appPaid/contact/page.tsx
+++ src/app/premium/appPaid/contact/page.tsx
@@ -42,53 +42,23 @@ export default function Contact(){
     }
     
     return(
-        // <div className="w-[100%] min-h-screen bg-gradient-to-br from-blue-300 via-blue-250 to-blue-100 flex flex-col justify-cente">
-        //     <div className="w-[70%] h-[50rem] bg-[#F9FAFB] flex flex-row m-auto rounded-xl">
-        //         <div className="w-[50%] relative text-center rounded-xl bg-green-200">
-        //             <h2 className="text-4xl mt-30">¡Envianos tu consulta!</h2>
-        //             <p className="w-80 mt-30 m-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat in, omnis facilis unde sunt eos fuga cumque velit? Asperiores obcaecati deleniti earum possimus. Quas ex dolore cum molestias, earum alias?</p>
-        //             <Image
-        //                 src="/images/logo/logo2.png"
-        //                 alt="MailImage"
-        //                 fill
-        //                 className="object-cover object-center mt-40"
-        //                 priority
-        //             />
-        //         </div>
-        //         <form onSubmit={handleSubmit} className="w-[50%] flex flex-col items-center justify-center">
-        //             <div className="w-full h-100 flex flex-col justify-center bg-yellow-200">
-        //                 <div className="flex flex-col items-center">
-        //                     <input type="text" name="name" placeholder="Name" className="mb-10 h-10 w-[70%] border-b-2 border-b-black focus:outline-none"/>
-        //                     <input type="text" name="email" placeholder="Email" className="mb-10 h-10 w-[70%] border-b-2 border-b-black focus:outline-none"/>
-                            
-        //                 </div>
-        //                 <label className="m-auto text-xl">Ingrese su mensaje</label>
-        //                 <textarea name="message" className="w-[80%] h-100 m-auto border-2"></textarea>
-        //             </div>
-        //             <button type="submit" disabled={loading} className="w-50 h-10 bg-orange-300 rounded-xl mt-30">{loading ? 'Enviando...' : 'Enviar'} </button>
-        //             {result && <p className="text-sm text-green-600 bg-yellow-200">{result}</p>}
-        //             {error && <p className="text-sm text-red-600 bg-yellow-200">{error}</p>}
-        //         </form> 
-        //     </div>
-        // </div>
-
         <div className="min-h-screen bg-gradient-to-br from-blue-300 via-blue-250 to-blue-100 flex flex-col justify-center py-12">
-            <div className="w-[70%] bg-[#F9FAFB] flex flex-row m-auto rounded-lg overflow-hidden h-[700px] shadow-xl shadow-black-500">
+            <div className="w-[95%] md:w-[70%] bg-[#F9FAFB] flex flex-col md:flex-row m-auto rounded-lg overflow-hidden h-auto md:h-[700px] shadow-xl shadow-black-500 p-2 md:p-0">
 
-                <div className="w-1/2 relative text-center p-10 rounded-xl m-2">
+                <div className=" w-full md:w-1/2 relative text-center p-5 md:p-10 rounded-xl m-auto md:m-2">
                     <div className="bg-[url('/images/general/img6.jpg')] bg-cover bg-center absolute inset-0 rounded-xl">
                         <div className="absolute inset-0 bg-black/50 rounded-xl"/>
                     </div>
 
-                    <div className="relative text-center p-10 z-20 text-white">
-                        <h2 className="text-6xl mt-6 z-20">¡Envianos tu consulta!</h2>
-                        <p className="w-80 mt-15 m-auto z-20 text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat in, omnis facilis unde sunt eos fuga cumque velit? ...</p>
+                    <div className="relative text-center p-5 md:p-10 z-20 text-white">
+                        <h2 className="text-4xl md:text-6xl mt-6 z-20">¡Envianos tu consulta!</h2>
+                        <p className="w-80 mt-15 m-auto z-20 text-xl md:text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat in, omnis facilis unde sunt eos fuga cumque velit? ...</p>
                     </div>
                 </div>
 
-                <form onSubmit={handleSubmit} className="w-1/2 flex flex-col items-center justify-center p-8">
+                <form onSubmit={handleSubmit} className="w-full md:w-1/2 flex flex-col items-center justify-center p-8">
 
-                    <div className="relative w-full h-[280px] mt-5 mb-10">
+                    <div className="relative w-full h-[150px] md:h-[280px] mt-0 md:mt-5 mb-5 md:mb-10">
                         <Image
                             src="/images/logo/logo2.png"
                             alt="MailImage"
@@ -99,15 +69,15 @@ export default function Contact(){
                     </div> 
                     <div className="w-full flex flex-col justify-center">
                         <div className="flex flex-col items-center">
-                            <input name="name" placeholder="Name" className="mb-10 h-10 w-[70%] border-b-2 border-b-black focus:outline-none"/>
-                            <input name="email" placeholder="Email" className="mb-10 h-10 w-[70%] border-b-2 border-b-black focus:outline-none"/>
+                            <input name="name" placeholder="Name" className="mb-5 md:mb-10 h-10 w-[85%] md:w-[70%] border-b-2 border-b-black focus:outline-none"/>
+                            <input name="email" placeholder="Email" className="mb-5 md:mb-10 h-10 w-[85%] md:w-[70%] border-b-2 border-b-black focus:outline-none"/>
                         </div>
 
                         <label className="block text-center mb-2 text-xl">Ingrese su mensaje</label>
-                        <textarea name="message" className="w-[80%] h-[180px] m-auto border-2"></textarea>
+                        <textarea name="message" className="w-full md:w-[80%] h-[180px] m-auto border-2"></textarea>
                     </div>
 
-                    <button type="submit" disabled={loading} className="w-40 h-20 bg-[#7ee8da] rounded-xl mt-6 hover:bg-[#7ee8da] hover:shadow-lg hover:shadow-blue-500/50">
+                    <button type="submit" disabled={loading} className="w-20 md:w-40 h-10 md:h-20 bg-[#7ee8da] rounded-xl mt-6 hover:bg-[#7ee8da] hover:shadow-lg hover:shadow-blue-500/50">
                         {loading ? 'Enviando...' : 'Enviar'}
                     </button>
 
